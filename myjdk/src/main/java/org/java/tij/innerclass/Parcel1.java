3f9dee20fd1f7ff6ff2c7d2c7399f0950fd097f0
transienthm <transienthm@vip.qq.com>
2017-09-15 17:02:10 +0800

    test

--- myjdk/src/main/java/org/java/tij/innerclass/Parcel1.java
+++ myjdk/src/main/java/org/java/tij/innerclass/Parcel1.java
@@ -21,5 +21,12 @@ public class Parcel1 {
 
     public void ship(String dest){
         Contents c = new Contents();
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcel1 p = new Parcel1();
+        p.ship("Tasmania");
     }
 }
